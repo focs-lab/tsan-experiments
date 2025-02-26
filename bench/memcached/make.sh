374f00781024505031dbacb7e8764f552ea279d8
Andrey Kogutenko <29797602+kogutenko@users.noreply.github.com>
2025-02-27 02:16:40 +0300

    Add benchmark tool and Memcached build & run script (#1)

---
+++ bench/memcached/make.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+MEMCACHED_THREADS=2
+MEMTIER_THREADS=8
+
+BENCH_DURATION=30
+BENCH_RUN_DELAY=3
+PORT=12345
+
+echo "memcached"
+
+echo
+echo " > memtier"
+echo "   Unpacking"
+
+tar --extract --file "$BENCH_SOURCE_DIR"/memtier_benchmark*.tar.gz --directory "$BENCH_BUILD_DIR"
+mv $(ls -d "$BENCH_BUILD_DIR"/memtier_benchmark-*.*/) "$BENCH_BUILD_DIR"/memtier-source
+
+echo "   Autoreconf"
+cd "$BENCH_BUILD_DIR"/memtier-source
+autoreconf --install --force "$BENCH_BUILD_DIR"/memtier-source > autoreconf.stdout.txt 2> autoreconf.stderr.txt
+
+echo "   Configuring"
+mkdir "$BENCH_BUILD_DIR"/memtier-build
+cd "$BENCH_BUILD_DIR"/memtier-build
+CFLAGS="-O2" "$BENCH_BUILD_DIR"/memtier-source/configure > configure.stdout.txt 2> configure.stderr.txt
+
+echo "   Compiling"
+make --jobs $(nproc) > build.stdout.txt 2> build.stderr.txt
+
+mkdir "$BENCH_BUILD_DIR/bin"
+cp "$BENCH_BUILD_DIR"/memtier-build/memtier_benchmark "$BENCH_BUILD_DIR/bin"
+
+echo
+echo " > Unpacking"
+
+tar --extract --file "$BENCH_SOURCE_DIR"/memcached*.tar.gz --directory "$BENCH_BUILD_DIR"
+mv $(ls -d "$BENCH_BUILD_DIR"/memcached-*.*/) "$BENCH_BUILD_DIR"/source
+
+echo
+echo " > Configuring"
+
+for PASS in $PASSES
+do
+    mkdir --parents "$BENCH_BUILD_DIR/build/$PASS"
+    cd "$BENCH_BUILD_DIR/build/$PASS"
+    CURRENT_CFLAGS="$CFLAGS"
+    [ "$PASS" = "tsan" ] && CURRENT_CFLAGS="$CFLAGS $FLAGS_TSAN"
+    [ "$PASS" = "tsan-new" ] && CURRENT_CFLAGS="$CFLAGS $FLAGS_TSAN_NEW"
+    printf "   %-10s" "$PASS"
+    { time CFLAGS="$CURRENT_CFLAGS" \
+        taskset -c "$BUILD_CORES" \
+         "$BENCH_BUILD_DIR/source"/configure > configure.stdout.txt 2> configure.stderr.txt; }
+done
+
+echo
+echo " > Compiling"
+
+for PASS in $PASSES
+do
+    printf "   %-10s" "$PASS"
+    cd "$BENCH_BUILD_DIR/build/$PASS"
+    { time taskset -c "$BUILD_CORES" \
+        make \
+        --jobs $(nproc) \
+        --directory "$BENCH_BUILD_DIR/build/$PASS" > "$PASS.stdout.log" 2> "$PASS.stderr.log"; }
+    cp "$BENCH_BUILD_DIR/build/$PASS/memcached" "$BENCH_BUILD_DIR/bin/$PASS"
+done
+
+echo
+echo " > Running"
+
+mkdir "$BENCH_BUILD_DIR/results"
+cd "$BENCH_BUILD_DIR/results"
+
+for PASS in $PASSES
+do
+    printf "   %-10s" "$PASS"
+    taskset -c "$RUN_CORES" \
+        "$BENCH_BUILD_DIR/bin/$PASS" \
+        --port "$PORT" \
+        --conn-limit 4096 \
+        --threads "$MEMCACHED_THREADS" > "$PASS.stdout.txt" 2> "$PASS.stderr.txt" &
+
+    PID=$!
+    sleep "$BENCH_RUN_DELAY"
+
+    taskset -c "$BENCH_CORES" \
+        "$BENCH_BUILD_DIR/bin/memtier_benchmark" \
+        --protocol memcache_text \
+        --hide-histogram \
+        --random-data \
+        --port "$PORT" \
+        --threads "$MEMTIER_THREADS" \
+        --test-time "$BENCH_DURATION" > "bench-$PASS.stdout.txt" 2> "bench-$PASS.stderr.txt"
+
+    kill "$PID"
+
+    grep "Totals" "bench-$PASS.stdout.txt" | awk '{print $2}' | tr '.' ' ' | awk '{print $1}'
+
+    sleep "$BENCH_RUN_DELAY"
+done
