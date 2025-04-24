9d8b5c32f87b7b1a6bb63c6bb18723b2ef4f79fe
Morthimer McMare <morthimer_second@list.ru>
2025-04-25 00:06:46 +0300

    Some small infrastructure additions to the MySQL benchmark scripts.
    Lua scripts in /usr/share/sysbench/ can now be selected.

--- sql/mysql/benchmysql/bench-rdonly-run.sh
+++ sql/mysql/benchmysql/bench-rdonly-run.sh
@@ -1,3 +1,6 @@
+SYSBENCH_SCRIPT_FILENAME="oltp_read_write.lua"
+
 source callmysql-export-main-vars.sh
 
-time sysbench "$SYSBENCH_SCRIPTS_DIR/oltp_read_only.lua" $SYSBENCH_CONNECTION_ARGS $SYSBENCH_RUN_ARGS run
+echo "Sysbench script: $SYSBENCH_SCRIPT_FILE"
+time sysbench "$SYSBENCH_SCRIPT_FILE" $SYSBENCH_CONNECTION_ARGS $SYSBENCH_RUN_ARGS run
