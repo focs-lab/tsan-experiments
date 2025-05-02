a2e43f187b126ea231f42deb592fee34e5efbed1
Alexey Paznikov <apaznikov@gmail.com>
2025-05-02 18:02:17 +0800

    Refactor memcached build script

--- nosql/memcached/link-memcached.sh
+++ nosql/memcached/link-memcached.sh
@@ -1,21 +1,72 @@
 #!/bin/sh
+set -e
 
-export LLVM_BUILD_DIR="/path/to/build-release-disable-instr"
+BUILD_TYPE=tsan
+
+# Build script for memcached with ThreadSanitizer
+# Requires LLVM_PATH environment variable to be set
+
+if [ -z "$LLVM_PATH" ]; then
+    echo "Error: LLVM_PATH environment variable is not set"
+    exit 1
+fi
+
+export LLVM_BUILD_DIR="$LLVM_PATH"
 export PATH="$LLVM_BUILD_DIR/bin:$PATH"
 
-rm -f *.ll
+cleanup() {
+    echo "Cleaning up temporary files..."
+    rm -f *.ll
+}
 
-CC=clang ./configure
+#trap cleanup EXIT
+cleanup
 
-for MODULE in "memcached" "hash" "jenkins_hash" "murmur3_hash" "slabs" "items" "assoc" "thread" \
-              "daemon" "stats_prefix" "util" "cache" "bipbuffer" "base64" "logger" "crawler" "itoa_ljust" \
-              "slab_automove" "authfile" "restart" "proto_text" "proto_bin" "extstore" "crc32c" "storage" \
-              "slab_automove_extstore"
-do
-    clang -DHAVE_CONFIG_H -I. -DNDEBUG -O2 -fsanitize=thread -g -S -emit-llvm -MT "memcached-$MODULE.o" \
-        -MD -MP -MF ".deps/memcached-$MODULE.Tpo" -c -o "memcached-$MODULE.ll" "$MODULE.c"
+echo "Setting up build environment..."
+CC=clang
+
+echo "Running configure..."
+./configure || {
+    echo "Configure failed"
+    exit 1
+}
+
+COMMON_FLAGS="-DHAVE_CONFIG_H -I. -DNDEBUG -O2 -g"
+if [ "$BUILD_TYPE" = "tsan" ]; then
+    COMMON_FLAGS="$COMMON_FLAGS -fsanitize=thread"
+fi
+
+compile_module() {
+    local module=$1
+#    echo "Compiling module: $module"
+    clang $COMMON_FLAGS -S -emit-llvm -MT "memcached-$module.o" \
+        -MD -MP -MF ".deps/memcached-$module.Tpo" -c -o "memcached-$module.ll" "$module.c"
+}
+
+MODULES="memcached hash jenkins_hash murmur3_hash slabs items assoc thread \
+         daemon stats_prefix util cache bipbuffer base64 logger crawler itoa_ljust \
+         slab_automove authfile restart proto_text proto_bin extstore crc32c storage \
+         slab_automove_extstore"
+
+echo "Compiling individual modules..."
+for MODULE in $MODULES; do
+    compile_module "$MODULE" || {
+        echo "Failed to compile module: $MODULE"
+        exit 1
+    }
 done
 
-llvm-link -S -o memcached.ll *.ll
+echo "Linking modules..."
+llvm-link -S -o memcached.ll *.ll || {
+    echo "Linking failed"
+    exit 1
+}
 
-clang -o memcached memcached.ll -O2 -g -levent "$LLVM_BUILD_DIR/lib/clang/19/lib/x86_64-unknown-linux-gnu/libclang_rt.tsan.a" -lm
+echo "Building final executable..."
+clang -o memcached memcached.ll $COMMON_FLAGS \
+    -levent "$LLVM_BUILD_DIR/lib/clang/19/lib/x86_64-unknown-linux-gnu/libclang_rt.tsan.a" -lm || {
+  echo "Final build failed"
+  exit 1
+}
+
+echo "Build completed successfully"
